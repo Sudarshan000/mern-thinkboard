82b9d8479e1419e8691289d2a3ca6ba4e34b506e
Sudarshan000 <sudarshanjha197@gmail.com>
2025-07-12 16:23:29 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 
 import noteRoutes from './routes/notesRoutes.js';
@@ -12,19 +13,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
-  }
-));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors(
+    {
+      origin: "http://localhost:5173",
+    }
+  ));
+}
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use(rateLimiter); // Apply rate limiting middleware
 
 app.use("/api/notes", noteRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(5000, () => {
